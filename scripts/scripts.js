f5e077df7eb15be5a4cc047c8c4fdff0aae2e1ac
ATerskin <terskinalex@gmail.com>
2018-05-27 20:10:04 -0400

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+console.log('hello Sasha');
+console.log('I want you to say hi to me');
+console.log(document)
+console.log(window.outerWidth)
+
+if (500 > window.outerWidth) {
+    // return(window.outerWidth)
+    console.log('This is mobile');
+// find the box
+  var boxElement = document.querySelector(".box")
+  console.log(boxElement)
+  boxElement.style.width = "80vw"
+// Set boxes width
+} 
+
+
+
+// // var element = document.querySelectorAll("blackBorder");
+// var element2 = document.querySelector(".blackBorder")
+// console.log(element2)
+
+// element2.style.backgroundColor = "red"
+// // element.styles.color = 'purple'; 
+
+// function myFunction() {
+//     document.querySelector(".example").style.backgroundColor = "red";
+// }
+
+// document.getElementById("clickButton").addEventListener ("click", myFunction);
